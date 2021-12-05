f0db1e3edce9f2fbd2f1188845aef55b0e19a68c
fb0801 <fbhatti0801@gmail.com>
2021-12-05 21:30:27 +0000

    react 2

--- react-essn/src/index.js
+++ react-essn/src/index.js
@@ -4,9 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+
+const greeting = <h1>Hello world!</h1>
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+   greeting
   </React.StrictMode>,
   document.getElementById('root')
 );
