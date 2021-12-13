b501cc1e946478d74186be77774810e2155bd2b9
fb0801 <fbhatti0801@gmail.com>
2021-12-13 21:20:07 +0000

    list component

--- react-essn/src/components/SeriesList/index.js
+++ react-essn/src/components/SeriesList/index.js
@@ -2,13 +2,17 @@ import React from "react";
 import Series from "../../containers/Series";
 import './index.css';
 
+const SeriesListItem = props => {
+<li key={series.show.id}>
+                        {series.show.name}</li>
+}
+
 const SeriesList = (props) =>{
     retrun (
         <div>
             <ul className="series-list">
                 {props.list.map(series => {
-                    <li key={series.show.id}>
-                        {series.show.name}</li>
+                    
                 })}
             </ul>
 
