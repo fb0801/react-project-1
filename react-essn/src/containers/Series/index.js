87c4341f22ae0b0100c1f1e4b04266b0b1a00a29
fb0801 <fbhatti0801@gmail.com>
2021-12-13 21:58:47 +0000

    conditional rendering 3

--- react-essn/src/containers/Series/index.js
+++ react-essn/src/containers/Series/index.js
@@ -10,10 +10,11 @@ class Series extends Component {
     
   
    onSeriesInputChange = e => {
+     //grabs the input query from the api and sees if input has any results
      this.setState({SeriesName: e.target.value, isfeteching: true, });
     fetch('https://api.tvmaze.com/search/shows?q=${e.target.value}')
     .then((Response)=> Response.json())
-    .then(json => this.setState({ series: json }));
+    .then(json => this.setState({ series: json, isfeteching:false }));
 
    }
 
@@ -21,13 +22,21 @@ class Series extends Component {
       const {series,SeriesName,isfeteching} = this.state;
     return (
         <div> 
-            the length of the series array - {this.state.series.length}
             <div>
               <input value={SeriesName}
               type="text" 
               onChange={this.onSeriesInputChange} />
             </div>
-
+            {
+            series.length ===0 && SeriesName.trim()=== ''
+            &&
+            <p>Please enter a tv show</p>
+            }
+            {
+              series.length === 0 && SeriesName.trim() !== ''
+              &&
+              <p>Nothing found</p>
+            }
             <SeriesList list={this.state.series.length} />
 </div>
     )
