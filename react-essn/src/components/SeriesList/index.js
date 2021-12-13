0158fb068920a1974e2b21f934979d3938c82cfc
fb0801 <fbhatti0801@gmail.com>
2021-12-13 21:23:00 +0000

    list component 2

--- react-essn/src/components/SeriesList/index.js
+++ react-essn/src/components/SeriesList/index.js
@@ -12,7 +12,7 @@ const SeriesList = (props) =>{
         <div>
             <ul className="series-list">
                 {props.list.map(series => {
-                    
+                    <SeriesListItem series={series}/>
                 })}
             </ul>
 
